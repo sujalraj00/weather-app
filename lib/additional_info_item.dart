0df1cae3c3395cc9c0c24c49135be980a612cc22
Sujal Raj <sujalraj611@gmail.com>
2024-10-11 16:11:54 +0530

    some functionalities added

---
+++ lib/additional_info_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AdditionalInfoItem extends StatelessWidget {
+  const AdditionalInfoItem({super.key,  required this.label,required this.icon, required this.value});
+  final IconData icon;
+  final String label;
+  final String value;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          size: 32,
+        ),
+        const SizedBox(height: 8,),
+        Text(label),
+        const SizedBox(height: 8,),
+        Text(
+          value,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold
+          ),
+        )
+      ],
+    );
+  }
+}
